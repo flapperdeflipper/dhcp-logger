8b36027407fbc34f30e456d13d74c7f3deb63659
Flip <flip@fliphess.com>
2019-12-28 12:59:52 +0100

    initial commit

---
+++ table.sql
@@ -0,0 +1,16 @@
+
+
+CREATE TABLE records (
+    id INT NOT NULL AUTO_INCREMENT,
+    active TINYINT(1) NOT NULL DEFAULT 0,
+    address VARCHAR(30) NOT NULL,
+    mac VARCHAR(30) NOT NULL,
+    hostname VARCHAR(50),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW(),
+    created_at TIMESTAMP NOT NULL,
+    PRIMARY KEY(id),
+    CONSTRAINT uc_address_name_mac UNIQUE (address,hostname,mac)
+);
+
+
+
