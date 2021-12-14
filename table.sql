a777c1348ff9a1fa5a75074c3bc38bdd3cbcf810
Flip Hess <2484666+fliphess@users.noreply.github.com>
2021-12-14 23:39:29 +0100

    Update table.sql

--- table.sql
+++ table.sql
@@ -1,11 +1,11 @@
 
 
 CREATE TABLE records (
-    id INT NOT NULL AUTO_INCREMENT,
-    active TINYINT(1) NOT NULL DEFAULT 0,
-    address VARCHAR(30) NOT NULL,
-    mac VARCHAR(30) NOT NULL,
-    hostname VARCHAR(50),
+    id         INT NOT NULL AUTO_INCREMENT,
+    active     TINYINT(1) NOT NULL DEFAULT 0,
+    address    VARCHAR(30) NOT NULL,
+    mac        VARCHAR(30) NOT NULL,
+    hostname   VARCHAR(50),
     updated_at TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW(),
     created_at TIMESTAMP NOT NULL,
     PRIMARY KEY(id),
